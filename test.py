0616025b2f7423b16a27c7d2e4c0ba3b278722fb
Smith0988 <chungdv.mta@gmail.com>
2023-11-20 14:41:59 +0700

    done

---
+++ test.py
@@ -0,0 +1,15 @@
+def remove_duplicate_lines(input_file, output_file):
+    # Mở file đầu vào để đọc
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+
+    # Loại bỏ các dòng trùng lặp
+    unique_lines = list(set(lines))
+
+    # Mở file đầu ra để ghi
+    with open(output_file, 'w') as file:
+        # Ghi các dòng không trùng lặp vào file mới
+        file.writelines(unique_lines)
+
+# Thực hiện loại bỏ dòng trùng lặp từ file text.txt và ghi vào file mới unique_text.txt
+remove_duplicate_lines('text.txt', 'unique_text.txt')
